02d0409170081bd945c7983957c6970f0664d062
Lorenzo Dal'Aqua <lorenzodalaqua@gmail.com>
2021-06-24 10:43:08 -0300

    Fixed css watching definitely now and added normalize styles.

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,12 @@
-const pluginSass = require("eleventy-plugin-sass");
+const pluginSass = require('eleventy-plugin-sass');
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.setUseGitIgnore(false); // Check the css folder
+
   eleventyConfig.addPlugin(pluginSass, {
     watch: ['sass/*', '!node_modules/**'],
     outputDir: './css'
   });
-  eleventyConfig.addWatchTarget("./scss/");
-  eleventyConfig.setBrowserSyncConfig({
-		files: './_site/css/**/*.css'
-	});
 
   eleventyConfig.addPassthroughCopy('img');
   eleventyConfig.addPassthroughCopy('css');
